88684f0369064e65b37765fabf36b608db7803c9
PatrickEversBW <p.evers@bestwaycorp.de>
2021-07-26 10:12:10 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react"
+import { Typography, CssBaseline, AppBar } from '@material-ui/core';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar color="inherit">
+        <Typography variant="h6">Hello World!</Typography>
+      </AppBar>
+    </div>
+  );
+}
+
+export default App;
